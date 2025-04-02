fbc5310fe3ba9045a5ec7f6639e5c3a93b0dbbfc
Martin Karugaba <martinkarugaba21@gmail.com>
2025-04-02 17:39:28 +0300

    Add theme selector label

--- src/features/themes/components/theme-selector.tsx
+++ src/features/themes/components/theme-selector.tsx
@@ -15,9 +15,20 @@ export function ThemeSelector() {
 
   return (
     <Select value={activeTheme} onValueChange={setActiveTheme}>
-      <SelectTrigger size="sm" className="w-32">
+      {/* <SelectTrigger size="sm" className="w-32">
         <SelectValue placeholder="Select a theme" />
-      </SelectTrigger>
+      </SelectTrigger>   */}
+      <SelectTrigger
+          id="theme-selector"
+          size="sm"
+          className="justify-start *:data-[slot=select-value]:w-12"
+        >
+          <span className="text-muted-foreground hidden sm:block">
+            Select a theme:
+          </span>
+          <span className="text-muted-foreground block sm:hidden">Theme</span>
+          <SelectValue placeholder="Select a theme" />
+        </SelectTrigger>
       <SelectContent align="end">
         {THEMES.map((theme) => (
           <SelectItem key={theme.name} value={theme.value}>
