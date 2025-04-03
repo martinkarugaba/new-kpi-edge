4fdb46e7737c9285fb98928d243f9044d5992f21
Martin Karugaba <martinkarugaba21@gmail.com>
2025-04-03 11:43:40 +0300

    Change default theme to amber

--- src/features/themes/components/theme-selector.tsx
+++ src/features/themes/components/theme-selector.tsx
@@ -1,34 +1,26 @@
-"use client"
+"use client";
 
-import { THEMES } from "../lib/themes"
-import { useThemeConfig } from "./active-theme"
+import { THEMES } from "../lib/themes";
+import { useThemeConfig } from "./active-theme";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
 export function ThemeSelector() {
-  const { activeTheme, setActiveTheme } = useThemeConfig()
+  const { activeTheme, setActiveTheme } = useThemeConfig();
 
   return (
     <Select value={activeTheme} onValueChange={setActiveTheme}>
-      {/* <SelectTrigger size="sm" className="w-32">
-        <SelectValue placeholder="Select a theme" />
-      </SelectTrigger>   */}
       <SelectTrigger
-          id="theme-selector"
-          size="sm"
-          className="justify-start *:data-[slot=select-value]:w-12"
-        >
-          <span className="text-muted-foreground hidden sm:block">
-            Select a theme:
-          </span>
-          <span className="text-muted-foreground block sm:hidden">Theme</span>
-          <SelectValue placeholder="Select a theme" />
-        </SelectTrigger>
+        size="sm"
+        className="w-32 bg-background text-foreground border"
+      >
+        <SelectValue placeholder="Select a theme" />
+      </SelectTrigger>
       <SelectContent align="end">
         {THEMES.map((theme) => (
           <SelectItem key={theme.name} value={theme.value}>
@@ -37,5 +29,5 @@ export function ThemeSelector() {
         ))}
       </SelectContent>
     </Select>
-  )
+  );
 }
